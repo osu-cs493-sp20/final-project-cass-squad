5fbcfda2e84ca954ba6431779ea8651b0892464d
coulbynguyen <nguyenco@oregonstate.edu>
2020-06-07 20:04:13 -0700

    finished GET /courses, POST /courses, GET /courses/:id, DELETE /courses/:id. Added some init courses to the db init. altered the docker-compose to include some standard. Changed the server.js to include connect to Db on start

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const api = require('./api');
+const morgan = require('morgan');
 
 const app = express();
 const port = process.env.PORT || 8000;
 
+
+const { connectToDB } = require('./lib/mongo');
+
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.static('public'));
+
+
 app.use('/', api);
 
 app.use('*', function (req, res, next) {
+  console.log("====== route does not exist");
   res.status(404).json({
     error: "Requested resource " + req.originalUrl + " does not exist"
   });
 });
 
-app.listen(port, function() {
-  console.log("Server is running on port", port);
+connectToDB(async () => {
+  // await connectToRabbitMQ('images');
+  app.listen(port, () => {
+    console.log("== Server is running on port", port);
+  });
 });
